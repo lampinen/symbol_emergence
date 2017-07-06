04452d95483a118e8d0c101577dd000d1b27a35c
Andrew Lampinen <lampinen@stanford.edu>
2017-07-05 20:56:19 -0700

    Removing extraneous exit...

--- recurrent/recurrent_cyclic_groups.py
+++ recurrent/recurrent_cyclic_groups.py
@@ -69,9 +69,6 @@ for nelements in xrange(1,RNN_seq_length+1):
 y_data = numpy.array([combine_list(x) for x in raw_x_data])
 x_data = numpy.array(map(lambda l: map(lambda x: numpy.argmax(x),l),raw_x_data),dtype=numpy.int32)
 masks = numpy.array(masks,dtype=numpy.bool)
-print x_data
-print len(x_data),group_order+group_order**2+group_order**3+group_order**4+group_order**5
-exit()
 
 for rseed in xrange(num_runs):
     print "run %i" %rseed
